e2a5522392fb5bb93d8396ec09d4d94386f16a73
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-25 04:57:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+class CashRegister
 
+attr_accessor :total, :discount
+
+
+
+def initialize (discount = 0)
+@total = 0
+@discount = discount
+@items = Array.new
+end
+
+def total
+@total
+end
+
+def add_item (title, price, quantity = 1)
+quantity.times {@items << title}
+@total += price * quantity
+end
+
+def apply_discount
+  if @discount == 0
+    puts "There is no discount to apply."
+  else
+minus = @total * @discount / 100
+@total = @total - minus
+end
+end
+
+def items
+@items
+end
+
+def void_last_transaction
+
+end
+
+end
